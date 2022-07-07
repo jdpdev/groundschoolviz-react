1672f04d626d7d6c9a155a4dcbb2c29d9a4661b0
gallahad@me.com <eYgbc6sc6GehM;XafYmUdUC#7mnUNGn>
2022-07-06 22:03:04 -0400

    altimeter setting demo

--- src/three/qnh/components/QNHAltimeter.tsx
+++ src/three/qnh/components/QNHAltimeter.tsx
@@ -1,3 +1,5 @@
+import { faArrowDown, faArrowUp } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React, { useState } from 'react'
 import { Button } from '../../../ui/Button'
 import { ControlBar } from '../../../ui/ControlBar'
@@ -10,6 +12,7 @@ import './css/QNHAltimeter.css'
 
 export function QNHAltimeter(props: LessonDialogProps) {
     const [setting, setSetting] = useState(29.92)
+    const altitude = (setting - 28) / 4
 
     return (
         <>
@@ -28,14 +31,39 @@ export function QNHAltimeter(props: LessonDialogProps) {
             <PageLayoutBottomSlot>
                 <div className='altimeter-demo'>
                     <div className='altimeter-container'>
+                        <div className='altitude-label'>
+                            Altitude
+                            
+                            <div style={{
+                                position: 'relative',
+                                left: '3.5em'
+                            }}>
+                                <FontAwesomeIcon icon={faArrowDown} />
+                            </div>
+                        </div>
                         <Altimeter
                             setting={setting}
-                            altitude={0.34}
+                            altitude={altitude}
                         />
+                        <div className='setting-label'>
+                            <div style={{
+                                position: 'relative',
+                                left: '6.5em'
+                            }}>
+                                <FontAwesomeIcon icon={faArrowUp} />
+                            </div>
+                            Altimeter Setting
+                        </div>
                     </div>
-                    <div className='setting-buttons'>
-                        <Button onClick={() => setSetting(setting + 0.14)}>+</Button>
-                        <Button onClick={() => setSetting(setting - 0.14)}>-</Button>
+                    <div className='right-container'>
+                        <div className='setting-buttons'>
+                            <Button onClick={() => setSetting(setting + 0.14)}>+</Button>
+                            <Button onClick={() => setSetting(setting - 0.14)}>-</Button>
+                        </div>
+                        <div>
+                            Change the altimeter setting<br/>
+                            to see the indicated altitude change.
+                        </div>
                     </div>
                 </div>
             </PageLayoutBottomSlot>
